47de1290e090526afe7d6517408cb9b6de1ba813
dengeist <eliasjmason@gmail.com>
2016-04-15 12:29:35 -0700

    Delete hard-coded object from array

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-var Contact = function(contactTemplate) { //gets the object that has been passed 
-    this.firstName = contactTemplate.firstName; 
+var Contact = function(contactTemplate) { //gets the object that has been passed
+    this.firstName = contactTemplate.firstName;
     this.lastName = contactTemplate.lastName;
     this.phoneNumber = contactTemplate.phoneNumber;
     this.street = contactTemplate.street;
@@ -17,7 +17,7 @@ var pushContact = function(newContact, contactsArray){
 var formListener = function(contactsArray) {
     $('form').on('submit', function(e) {
         e.preventDefault();
-        var newContact, 
+        var newContact,
             contactTemplate = { //captures the information from the form
             firstName: $('input[name="firstName"]').val(),
             lastName: $('input[name="lastName"]').val(),
@@ -26,7 +26,7 @@ var formListener = function(contactsArray) {
             city: $('input[name="city"]').val(),
             state: $('input[name="state"]').val()
         };
-        // run contactMaker on template object; 
+        // run contactMaker on template object;
         // store result it in variable
         newContact = new Contact(contactTemplate);
         pushContact(newContact, contactsArray);
@@ -34,39 +34,6 @@ var formListener = function(contactsArray) {
 };
 
 $(document).ready(function() {
-    var contactsArray = [{
-        contactName: {
-            first: "Anne",
-            last: "Perkins"
-        },
-        contactPhoneNumber: 5555555,
-        contactAddress: {
-            street: "478 Maple Rd",
-            city: "Pawnee",
-            state: "IN"
-        }
-    }, {
-        contactName: {
-            first: "Andy",
-            last: "Dwyer"
-        },
-        contactPhoneNumber: 5555556,
-        contactAddress: {
-            street: "479 Maple Rd",
-            city: "Pawnee",
-            state: "IN"
-        }
-    }, {
-        contactName: {
-            first: "Donna",
-            last: "Meagle"
-        },
-        contactPhoneNumber: 5555557,
-        contactAddress: {
-            street: "480 Maple Rd",
-            city: "Pawnee",
-            state: "IN"
-        }
-    }];
+    var contactsArray = [];
     formListener(contactsArray);
 });
