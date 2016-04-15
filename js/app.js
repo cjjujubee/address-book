c270a922908f9bff71c200ca8f1452eb20ff9989
dengeist <eliasjmason@gmail.com>
2016-04-15 08:15:43 -0700

    stub out contact object

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
-    
+    var contactsArray = [{
+        contactFirstName: "Anne",
+        contactLastName: "Perkins",
+        contactPhoneNumber: 5555555,
+        contactAddress: {
+            street: "478 Maple Rd",
+            city: "Pawnee",
+            state: "IN"
+        }
+    },
+    {
+        contactFirstName: "Anne",
+        contactLastName: "Perkins",
+        contactPhoneNumber: 5555555,
+        contactAddress: {
+            street: "478 Maple Rd",
+            city: "Pawnee",
+            state: "IN"
+        }
+    },
+    {
+        contactFirstName: "Anne",
+        contactLastName: "Perkins",
+        contactPhoneNumber: 5555555,
+        contactAddress: {
+            street: "478 Maple Rd",
+            city: "Pawnee",
+            state: "IN"
+        }
+    }];
 });
